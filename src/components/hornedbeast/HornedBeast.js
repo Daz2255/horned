0e80c5d77fe94b866077488fc6397e5dcb31e8a8
Darren <hldaz77@gmail.com>
2023-07-18 14:41:15 +0100

    lab completed

--- src/components/hornedbeast/HornedBeast.js
+++ src/components/hornedbeast/HornedBeast.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./HornedBeast.css";
 
-export default function HornedBeasts({ title, imgUrl, altText, description }) {
+export default function HornedBeast({ data }) {
+  const [favorites, setFavorites] = useState(0);
+
+  const handleFavoriteClick = () => {
+    setFavorites((prevFavorites) => prevFavorites + 1);
+  };
+
   return (
-    <div className="beast">
-      <h2>{title}</h2>
-      <img src={imgUrl} alt={altText} title={title} />
-      <p>{description}</p>
+    <div>
+      <h3>{data.title}</h3>
+      <img
+        src={data.image_url}
+        alt={data.title}
+        onClick={handleFavoriteClick}
+      />
+      <p>{data.description}</p>
+      <span>{favorites} Favorites</span>
+      <button onClick={handleFavoriteClick}>
+        <span role="img" aria-label="heart">
+          ❤️
+        </span>
+      </button>
     </div>
   );
 }
